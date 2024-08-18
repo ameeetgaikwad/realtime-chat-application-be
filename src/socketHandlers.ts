f8131ebd9a74927c7b7377ee1bc1efc55cc19900
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-08-18 10:21:20 +0530

    read message, online, typing functionality

--- src/socketHandlers.ts
+++ src/socketHandlers.ts
@@ -1,16 +1,16 @@
 import { Socket, Server } from "socket.io";
 import { db } from "./drizzle/db";
 import { users, conversations, messages, typing } from "./drizzle/schema";
-import { eq, and, or, desc, lt, ne, asc } from "drizzle-orm";
+import { eq, and, or, desc, lt, ne, asc, inArray, sql } from "drizzle-orm";
 
 export const setupSocketHandlers = (io: Server) => {
   io.on("connection", (socket: Socket) => {
     console.log("A user connected");
-
+    let USER_ID: number;
     socket.on(
       "join",
       async (email: string, firstName: string, lastName: string) => {
-        console.log("join");
+        console.log("join", email);
         try {
           const existingUsers = await db
             .select()
@@ -37,13 +37,15 @@ export const setupSocketHandlers = (io: Server) => {
               .where(eq(users.id, user.id));
           }
 
-          socket.data.userId = user.id;
+          io.emit("userOnline", true, user.id);
+
+          USER_ID = user.id;
 
           socket.emit("joined", user);
 
           const userConversations = await getUserConversations(user.id);
           socket.emit("conversationList", userConversations);
-
+          console.log("fetching users");
           // Fetch all users except the current user
           const allUsers = await db
             .select()
@@ -65,56 +67,85 @@ export const setupSocketHandlers = (io: Server) => {
           .where(eq(messages.conversationId, conversationId))
           .orderBy(desc(messages.createdAt))
           .limit(20);
-        console.log("recentMessages", recentMessages);
         socket.emit("conversationReady", conversationId, recentMessages);
       } catch (error) {
         console.error("Error getting conversation:", error);
       }
     });
 
-    socket.on("getOrCreateConversation", async (otherUserId: number) => {
+    socket.on("getLatestMessages", async (userId: number) => {
       try {
-        const userId = socket.data.userId;
-        let conversation;
+        const userConversations = await getUserConversations(userId);
+        const recentMessagesPromises = userConversations.map(
+          async (conversation) => {
+            const recentMessages = await db
+              .select()
+              .from(messages)
+              .where(eq(messages.conversationId, conversation.id))
+              .orderBy(desc(messages.createdAt))
+              .limit(1);
 
-        const existingConversations = await db
-          .select()
-          .from(conversations)
-          .where(
-            or(
-              and(
-                eq(conversations.user1Id, userId),
-                eq(conversations.user2Id, otherUserId)
-              ),
-              and(
-                eq(conversations.user1Id, otherUserId),
-                eq(conversations.user2Id, userId)
-              )
-            )
-          );
+            return {
+              conversationId: conversation.id,
+              recentMessage: recentMessages[0],
+            };
+          }
+        );
+        const resolvedRecentMessages = await Promise.all(
+          recentMessagesPromises
+        );
 
-        if (existingConversations.length === 0) {
-          const newConversations = await db
-            .insert(conversations)
-            .values({ user1Id: userId, user2Id: otherUserId })
-            .returning();
-          conversation = newConversations[0];
-        } else {
-          conversation = existingConversations[0];
-        }
-
-        const recentMessages = await db
-          .select()
-          .from(messages)
-          .where(eq(messages.conversationId, conversation.id))
-          .orderBy(asc(messages.createdAt))
-          .limit(20);
-        let conversationId = conversation.id;
-        socket.emit("conversationReady", { conversationId, recentMessages });
+        io.emit("recentMessages", resolvedRecentMessages);
       } catch (error) {
-        console.error("Error getting or creating conversation:", error);
+        console.error("Error getting all conversations:", error);
       }
     });
+
+    socket.on(
+      "getOrCreateConversation",
+      async (otherUserId: number, userId: number) => {
+        try {
+          let conversation;
+
+          const existingConversations = await db
+            .select()
+            .from(conversations)
+            .where(
+              or(
+                and(
+                  eq(conversations.user1Id, userId),
+                  eq(conversations.user2Id, otherUserId)
+                ),
+                and(
+                  eq(conversations.user1Id, otherUserId),
+                  eq(conversations.user2Id, userId)
+                )
+              )
+            );
+
+          if (existingConversations.length === 0) {
+            const newConversations = await db
+              .insert(conversations)
+              .values({ user1Id: userId, user2Id: otherUserId })
+              .returning();
+            conversation = newConversations[0];
+          } else {
+            conversation = existingConversations[0];
+          }
+
+          const recentMessages = await db
+            .select()
+            .from(messages)
+            .where(eq(messages.conversationId, conversation.id))
+            .orderBy(asc(messages.createdAt))
+            .limit(20);
+          let conversationId = conversation.id;
+          socket.emit("conversationReady", { conversationId, recentMessages });
+        } catch (error) {
+          console.error("Error getting or creating conversation:", error);
+        }
+      }
+    );
 
     socket.on(
       "message",
@@ -134,6 +165,7 @@ export const setupSocketHandlers = (io: Server) => {
               content,
               mediaUrl,
               mediaType,
+              isRead: false,
             })
             .returning();
 
@@ -156,6 +188,16 @@ export const setupSocketHandlers = (io: Server) => {
             io.emit(recipientId.toString(), message[0]);
 
             socket.emit("messageSent", message[0]);
+
+            // Emit an event for unread message count
+            // const unreadCount = await getUnreadMessageCount(
+            //   conversationId,
+            //   recipientId
+            // );
+            // io.emit("unreadMessageCount", {
+            //   conversationId,
+            //   count: unreadCount,
+            // });
 
             const senderConversations = await getUserConversations(senderId);
             const recipientConversations = await getUserConversations(
@@ -183,29 +225,29 @@ export const setupSocketHandlers = (io: Server) => {
       }
     );
 
-    // socket.on("readMessages", async (conversationId: number, messageIds: number[]) => {
-    //   try {
-    //     await db
-    //       .update(messages)
-    //       .set({ isRead: true })
-    //       .where(
-    //         and(
-    //           eq(messages.conversationId, conversationId),
-    //           sql`${messages.id} = ANY(${messageIds})`
-    //         )
-    //       );
+    socket.on("readMessages", async (messageId: number) => {
+      try {
+        const message = await db
+          .update(messages)
+          .set({ isRead: true })
+          .where(eq(messages.id, messageId))
+          .returning();
 
-    //     const conversation = await db.select().from(conversations)
-    //       .where(eq(conversations.id, conversationId));
+        socket.emit("messagesRead", messageId, message[0].conversationId);
 
-    //     if (conversation.length > 0) {
-    //       const otherUserId = conversation[0].user1Id === socket.data.userId ? conversation[0].user2Id : conversation[0].user1Id;
-    //       io.to(otherUserId.toString()).emit("messagesRead", { conversationId, messageIds });
-    //     }
-    //   } catch (error) {
-    //     console.error("Error marking messages as read:", error);
-    //   }
-    // });
+        // Update unread count for the reader
+        // const unreadCount = await getUnreadMessageCount(
+        //   conversationId,
+        //   socket.data.userId
+        // );
+        // socket.emit("unreadMessageCount", {
+        //   conversationId,
+        //   count: unreadCount,
+        // });
+      } catch (error) {
+        console.error("Error marking messages as read:", error);
+      }
+    });
 
     // socket.on(
     //   "loadMoreMessages",
@@ -235,12 +277,13 @@ export const setupSocketHandlers = (io: Server) => {
 
     socket.on("disconnect", async () => {
       try {
-        if (socket.data.userId) {
+        console.log(USER_ID, "disconnected");
+        if (USER_ID) {
           await db
             .update(users)
             .set({ isOnline: false, lastSeen: new Date() })
-            .where(eq(users.id, socket.data.userId));
-          io.emit("userOffline", socket.data.userId);
+            .where(eq(users.id, USER_ID));
+          io.emit("userOnline", false, USER_ID);
         }
       } catch (error) {
         console.error("Error updating user status on disconnect:", error);
@@ -257,4 +300,18 @@ async function getUserConversations(userId: number) {
       or(eq(conversations.user1Id, userId), eq(conversations.user2Id, userId))
     )
     .orderBy(desc(conversations.lastMessageAt));
+}
+
+async function getUnreadMessageCount(conversationId: number, userId: number) {
+  const result = await db
+    .select({ count: sql`count(*)` })
+    .from(messages)
+    .where(
+      and(
+        eq(messages.conversationId, conversationId),
+        eq(messages.isRead, false),
+        ne(messages.senderId, userId)
+      )
+    );
+  return Number(result[0].count);
 }
